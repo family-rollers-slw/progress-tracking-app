c0748da8998ba31dc3e7ef9ae21fb398c9580d89
DanielRmz1 <daniel.1912@hotmail.com>
2023-05-06 01:05:42 -0600

    Add style linters and SASS support

--- client/src/views/FamilyRollers/index.tsx
+++ client/src/views/FamilyRollers/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 const FamilyRollers = () => {
-	return <div>Family Rollers Wep App</div>;
+	return <div className={styles.app}>Family Rollers Wep App</div>;
 };
 
 export default FamilyRollers;
